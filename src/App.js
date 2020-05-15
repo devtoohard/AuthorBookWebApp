dd26fe0acc30a5188b2908a00a10feb62a7ffb95
Shawn <devcorehd@gmail.com>
2020-05-16 02:21:58 +0800

    adding files

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import BookList from './components/BookList'
+import AddBook from './components/AddBook'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+
+
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+})
+
+function App() {
+  
+  return (
+        <ApolloProvider client = {client} >
+        <div id="main">
+          <h1>Shawn's reading list </h1>
+          <BookList />
+          <AddBook />
+        </div>
+        </ApolloProvider>
+  )
+}
+
+
+
+export default App;
